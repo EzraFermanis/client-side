1a1a79fad6fbcbcb4a27a60f502159315ad6032e
Ezra Fermanis & Tony Luisi <pair+ezrafermanis.tony-luisi@devbootcamp.com>
2016-04-04 10:23:19 +1200

    Updates

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var example = require('./views/example.hbs')
 xhr.get('https://api.wheretheiss.at/v1/satellites/25544', function(err, data) {
   if (err) console.log(err) // do something
 
-  console.log(data.body)
+  console.log(data.body.name)
   document.body.innerHTML = example(data.body);
 })
