a74787da12dae9f55b235c5980177fc171b7ec85
Ezra Fermanis & Tony Luisi <pair+ezrafermanis.tony-luisi@devbootcamp.com>
2016-04-04 12:06:22 +1200

    done

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 var xhr = require('xhr')
-var example = require('./views/example.hbs')
+var example = require('./views/cats.hbs')
+var $ = require('jquery')
 
-xhr.get('https://api.wheretheiss.at/v1/satellites/25544', function(err, data) {
-  if (err) console.log(err) // do something
+getInfo()
 
-  console.log(data.body)
-  document.body.innerHTML = example({ name: "Space" });
+$(document).ready(function() {
+  $('#submit').click(function() {
+    getInfo()
+  })
+  $('#getCat').click(function() {
+    getInfo($(''))
+  })
+
 })
+
+
+function getInfo(id){
+  if (id) {
+
+  }
+
+  xhr.get('http://localhost:3000/v1/cats', function(err, data) {
+
+    if (err) console.log(err) // do something
+
+    console.log(data.body)
+    console.log('test1')
+    document.body.innerHTML = example(JSON.parse(data.body));
+  })
+}
